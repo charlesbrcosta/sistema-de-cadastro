f17a1bfe83a4b3790dd48ab3ada47f0e2d06b84e
Charles Bruno <charlesbrcosta@gmail.com>
2022-09-18 09:36:25 -0300

    adcinando um console log

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
 console.log("Rodando um sistema de gerenciamento de jogos");
 console.log("Conexão com o banco de dados realizado com sucesso");
-
-
-let mostraNome;
-
-function imprimeNOme(nome){
-    mostraNome = nome;
-    return mostraNome;
-}
-
-console.log(imprimeNOme("Charles Bruno"))
+console.log("Treinando Git");
