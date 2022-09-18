756e7f854558ed1852b5a0395a2a835913a67091
charlesbrcosta <48035699+charlesbrcosta@users.noreply.github.com>
2022-09-18 09:14:27 -0300

    adicionado a função mostrarNome app.js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 console.log("Rodando um sistema de gerenciamento de jogos");
 console.log("Conexão com o banco de dados realizado com sucesso");
+
+
+let mostraNome;
+
+function imprimeNOme(nome){
+    mostraNome = nome;
+    return mostraNome;
+}
+
+console.log(imprimeNOme("Charles Bruno"))
